764b41a0c3755122fbd462787abaeb81f7171975
ebrahim salah <ebrahimsalah489@gmail.com>
2025-06-19 16:11:59 +0300

    chore(wishlist):edit validation

--- src/modules/wishlist/wishlist.validation.js
+++ src/modules/wishlist/wishlist.validation.js
@@ -8,21 +8,10 @@ export const toggleWishlistSchema = Joi.object({
   }),
 });
 
-// export const removeFromWishlistSchema = Joi.object({
-//   productId: Joi.string().length(24).required().messages({
-//     "string.base": "Product ID must be a string",
-//     "string.length": "Product ID must be 24 characters",
-//     "any.required": "Product ID is required",
-//   }),
-// });
-
-
-export const removeFromWishlistSchema = {
-  params: Joi.object({
-    productId: Joi.string().length(24).required().messages({
-      "string.base": "Product ID must be a string",
-      "string.length": "Product ID must be 24 characters",
-      "any.required": "Product ID is required",
-    }),
+export const removeFromWishlistSchema = Joi.object({
+  productId: Joi.string().length(24).required().messages({
+    "string.base": "Product ID must be a string",
+    "string.length": "Product ID must be 24 characters",
+    "any.required": "Product ID is required",
   }),
-};
+});
